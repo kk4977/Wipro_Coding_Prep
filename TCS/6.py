47e031b4029998c23017e46f3b83d1066fe9d9fb
SIVA KUMAR KADALI <kadalisivakumar789@gmail.com>
2025-03-13 01:28:46 +0530

    updated 6 th question  files

--- TCS/6.py
+++ TCS/6.py
@@ -7,7 +7,22 @@
 # Output: { "granite": 2, "basalt": 1, "shale": 1 }
 
 # input_list = 
-def frequency(input_list)
+def frequency(input_list):
+    frequency_map = {}
+    for word in input_list:
+        if word in frequency_map:
+            frequency_map[word] += 1
+        else:
+            frequency_map[word] = 1
+    return frequency_map               # output = {'granite': 2, 'basalt': 1, 'shale': 1}
+    
+    
+    # for key, value in frequency_map.items():   # output = granite: 2
+    #                                                     # basalt: 1
+    #                                                     # shale: 1
+    #     print(f"{key}: {value}")
+
+
 input_str =  ["granite", "basalt", "granite", "shale"]
 input_list = list(map(str, input_str))
-print(input_list)
+(frequency(input_list))
